0453369af4d54db98f6e1e9d73623c01f16e4dfa
Don Alvin <alvindon41@gmail.com>
2024-07-01 20:15:00 +0300

    footer and contact us

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -1,7 +1,30 @@
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+import { Label } from "../ui/label"
+import { Textarea } from "../ui/textarea"
+
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className="p-6 bg-slate-100">
+      <div>
+        <h3 className="font-semibold text-2xl">Looking to sell or buy?</h3>
+        <p className="text-xl text-center">Tell us more about your dreams or project and we will get back to you </p>
+      </div>
+      <form className="grid gap-3" method="POST" action="https://formsubmit.co/alvindon41@gmail.com">
+        <div className="grid gap-2">
+          <Label htmlFor='name'>Name</Label>
+          <Input className='rounded p-3' id='name' type='text' name='name' required />
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor='email'>Email</Label>
+          <Input className='rounded p-3' id='email' type='email' required name='email' />
+        </div>
+        <Textarea placeholder='Enter your message here..' id='message' rows='10' required />
+        <Button className='text-white bg-secondary w-fit rounded p-2'>Send message</Button>
+      </form>
+    </section>
   )
 }
 
 export default Contact
+
--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,6 +1,28 @@
+import { FacebookIcon, Instagram, LinkedinIcon, Mail, Phone } from "lucide-react"
+import { Link } from "react-router-dom"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-primary text-white p-6">
+      <div className="grid gap-3">
+        <h4 className="font-semibold text-xl">Contact us</h4>
+        <div className="grid gap-3">
+          <div className="flex gap-2">
+            <Phone />
+            <span>+254 704 087097</span>
+          </div>
+          <div className="flex gap-2">
+            <Mail />
+            <span>alvindon41@gmail.com</span>
+          </div>
+        </div>
+        <div className="flex gap-3">
+          <Link className="p-2 rounded-full bg-green-900"><FacebookIcon /></Link>
+          <Link className="p-2 rounded-full bg-green-900"><LinkedinIcon /></Link>
+          <Link className="p-2 rounded-full bg-green-900"><Instagram /></Link>
+        </div>
+      </div>
+    </footer>
   )
 }
 
