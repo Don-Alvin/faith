69be9e468e88d66779838dd2ff9b140a0f174553
Don Alvin <alvindon41@gmail.com>
2024-07-14 09:54:13 +0300

    Logo update

--- src/components/Layout/MainNav.jsx
+++ src/components/Layout/MainNav.jsx
@@ -36,7 +36,7 @@ const MainNav = () => {
     <div className={`flex justify-between p-4 ${isActive ? "bg-primary" : ''}`}>
       <div className='flex items-center text-yellow-600 font-semibold'>
         <Home />
-        <span className='text-xl'>faith</span>
+        <span className='text-md'>Lamona Realtors</span>
       </div>
       <div className='px-10'>
           <div className={`flex gap-4 ${isActive ? 'text-yellow-600' : 'text-white'} font-semibold`}>
