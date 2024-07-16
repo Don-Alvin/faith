607aaece9554d6a03252c3f4ec234b371dd2cb72
Don Alvin <alvindon41@gmail.com>
2024-07-16 15:02:48 +0300

    order listings

--- src/api/listingsApi.js
+++ src/api/listingsApi.js
@@ -1,13 +1,14 @@
-import { collection, getDocs, query } from "firebase/firestore"
-import { db } from "./firebase"
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import { db } from "./firebase";
 
-export const getListings = async() => {
-    try{
-        const q = query(collection(db, "listings"))
-        const response = await getDocs(q)
-        const listings = response.docs.map((document) => document.data())
-        return listings
-    }catch (error){
-        throw new Error('Could not fetch listings')
-    }
-}
+export const getListings = async () => {
+  try {
+    const q = query(collection(db, "listings"), orderBy("id", "desc"));
+    const response = await getDocs(q);
+    const listings = response.docs.map((document) => document.data());
+    return listings;
+  } catch (error) {
+    console.error("Error fetching listings:", error);
+    throw new Error("Could not fetch listings");
+  }
+};
