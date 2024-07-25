aaa77b4a6f443068f0d3698937220ff2faad10e8
Don Alvin <alvindon41@gmail.com>
2024-07-25 08:22:00 +0300

    form submission

--- src/components/Forms/BookForm.jsx
+++ src/components/Forms/BookForm.jsx
@@ -1,30 +1,37 @@
-import React from 'react'
+import { useState } from 'react'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
+import 'react-international-phone/style.css';
 
 const BookForm = ({site}) => {
+
   return (
-    <form className='w-full flex flex-col items-center gap-4' action="https://formsubmit.co/anadoomollo@zohomail.com">
+    <form className='w-full flex flex-col items-center gap-4' method= 'POST' action="https://formsubmit.co/anadoomollo@zohomail.com">
         <div className='w-full md:w-[60%] flex flex-col gap-4'>
           <div  className='grid gap-2 w-full'>
             <Label htmlFor='name'>Enter your full name</Label>
-            <Input className='border border-black rounded h-10 p-1' required name='name' type='text' id='name' />
+            <Input className='border border-slate-400 rounded h-10 p-1' required name='name' type='text' id='name' />
         </div>
         <div  className='grid gap-2 w-full'>
             <Label htmlFor='name'>Enter your email</Label>
-            <Input className='border border-black rounded h-10 p-1' required name='email' type='email' id='email' />
+            <Input className='border border-slate-400 rounded h-10 p-1' required name='email' type='email' id='email' />
+        </div>
+        <div  className='grid gap-2 w-full'>
+            <Label htmlFor='number'>Enter your phone number</Label>
+            <Input 
+              className='border border-slate-400 rounded h-10 p-1' required name='number' type='tel' id='email' />
         </div>
         <div  className='grid gap-2 w-full'>
             <Label htmlFor='date'>Pick date of visit</Label>
-            <Input className='border border-black rounded h-10 p-1' required name='date' type='date' id='date' />
+            <Input className='border border-slate-400 rounded h-10 p-1' required name='date' type='date' id='date' />
         </div>
         <div  className='grid gap-2 w-full'>
             <Label htmlFor='site'>Site to visit</Label>
-            <Input className='border border-black rounded h-10 p-1' required name='site' type='site' id='site' />
+            <Input className='border border-slate-400 rounded h-10 p-1' required name='site' type='site' id='site' />
         </div>  
         </div>
-        <Button className='bg-secondary text-white p-2 rounded' value='send'>Submit</Button>
+        <Button className='bg-secondary text-white p-2 rounded'>Book Viewing</Button>
     </form>
   )
 }
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const ErrorPage = () => {
   return (
     <div>
+      <MetaData title={'Page not found'} />
         <p>Ooops, we could not find the page you are looking for!</p>
         <p>Click <Link to='/'>here</Link> to return to homepage.</p>
     </div>
